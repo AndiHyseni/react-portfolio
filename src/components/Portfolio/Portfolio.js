1e56f288780023ee27f0b5bcaf25c89e75ab0d8d
AndiHyseni <hyseniandi6@gmail.com>
2022-04-08 21:25:26 +0200

    Portfolio commit

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,10 +1,57 @@
-import React from 'react';
-import "./Portfolio.css"; 
+import React from "react";
+import "./Portfolio.css";
+import { AiFillGithub } from "react-icons/ai";
+import IMG1 from "../../assets/Clothes.PNG";
+import IMG2 from "../../assets/Meals.PNG";
+import IMG3 from "../../assets/Expense.PNG";
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Premium Clothes",
+    github: "https://github.com/AndiHyseni/PremClothes",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Order Food",
+    github: "https://github.com/AndiHyseni/Meals",
+  },
+
+  {
+    id: 3,
+    image: IMG3,
+    title: "Expenses",
+    github: "https://github.com/AndiHyseni/ExpenseForm",
+  },
+];
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
-  )
+    <section id="portfolio">
+      <h5>My recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {data.map(({ id, image, title, github }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target="_blank">
+                  <AiFillGithub /> GitHub
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import "./Testimonials.css";
+import I1 from '../../assets/Alma.jpg';
+import I2 from '../../assets/Ardi.jpg';
+import I3 from '../../assets/Endrit.jpg';
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className='container testimonials__container'>
+        <article className='testimonial'>
+          <div className='client__avatar'>
+            <img src={I1} alt='Alma' />
+            <h5>Alma Bashiq</h5>
+            <small className='client__review'></small>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 };
 
