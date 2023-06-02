d020ed7b4455b3372fde06ea7c9edf7787875b55
unknown <hyseniandi6@gmail.com>
2023-06-02 21:01:05 +0200

    Send Email

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 import "./Contact.css";
-import {MdOutlineEmail} from 'react-icons/md';
-import {RiMessengerLine} from 'react-icons/ri';
-import {BsWhatsapp} from 'react-icons/bs';
-import { useRef } from 'react';
-import emailjs from 'emailjs-com';
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
   const form = useRef();
@@ -12,47 +12,73 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_vxh3y68', 'template_97otvxo', form.current, 'kUv6Hab8BbSsijONb');
+    emailjs.sendForm(
+      "service_4al204q",
+      "template_97otvxo",
+      form.current,
+      "kUv6Hab8BbSsijONb"
+    );
 
     e.target.reset();
   };
 
   return (
-    <section id='contact'>
+    <section id="contact">
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
 
-      <div className='container contact__container'>
-        <div className='contact__options'>
-          <article className='contact__option'>
-            <MdOutlineEmail className='contact__option-icon' />
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>hyseniandi6@gmail.com</h5>
-            <a href='mailto:hyseniandi6@gmail.com' target="_blank">Send a message</a>
+            <a href="mailto:hyseniandi6@gmail.com" target="_blank">
+              Send a message
+            </a>
           </article>
-          <article className='contact__option'>
-            <RiMessengerLine className='contact__option-icon' />
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon" />
             <h4>Messenger</h4>
             <h5>Andi Hyseni</h5>
-            <a href='https://m.me/anndi.hyseni' target="_blank">Send a message</a>
+            <a href="https://m.me/anndi.hyseni" target="_blank">
+              Send a message
+            </a>
           </article>
-          <article className='contact__option'>
-            <BsWhatsapp className='contact__option-icon' />
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
             <h4>WhatsApp</h4>
             <h5>+38349377193</h5>
-            <a href='https://api.whatsapp.com/send?phone=+38349377193' target="_blank">Send a message</a>
+            <a
+              href="https://api.whatsapp.com/send?phone=+38349377193"
+              target="_blank"
+            >
+              Send a message
+            </a>
           </article>
         </div>
 
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name='name' placeholder='Your Full Name' required />
-          <input type='email' name='email' placeholder='Your Email' required />
-          <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
-          <button type='submit' className='btn btn-primary'>Send Message</button>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
         </form>
       </div>
     </section>
-  )
+  );
 };
 
 export default Contact;
